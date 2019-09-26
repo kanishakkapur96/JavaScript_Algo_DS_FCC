9dbd13a0723d0ad2711864f18b85d08d7a3d4a99
Kanishak Kapur <kanishak.kapur@startupfarms.corp>
2019-09-26 16:22:00 +0530

    Solutions for Basic JS Challenges

---
+++ ComplexObjects.js
@@ -0,0 +1,19 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist":"Taylor Swift",
+      "title":"Bad boy",
+      "formats":["CD","Casset"],
+      "release_year":1988
+    }
+  ];
